ea343b2d0f4b85c2d94f5fc3b019f74b155fb4b2
Alexia Ashton <ashtonalexia@gmail.com>
2021-03-19 12:20:57 +0100

    delete functioning

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -15,16 +15,16 @@ class DosesController < ApplicationController
     else
       render :new
     end
-
-    def destroy
-      @dose = Dose.find(params[:id])
-      @dose.destroy
-      redirect_to cocktail_path(@dose.cocktail)
-    end
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
 
   private
+
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses, only: [ :create]
   end
-  resources :doses, only: [ :delete ]
+  resources :doses, only: [ :destroy ]
 end
